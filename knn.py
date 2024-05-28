5569a84ecfc1c4421cccfaa612e763a62cdfcf53
Sarthak Arora <130503783+sarthakkarora@users.noreply.github.com>
2024-05-28 23:47:44 +0530

    knn.py

--- knn.py
+++ knn.py
@@ -1,45 +1,54 @@
 import numpy as np
 from statistics import mode
+from typing import List, Tuple
+import pandas as pd
 
 class KNN:
-    def __init__(self, train_set, test_set):
-        self.train_set = train_set
-        self.test_set = test_set
-    
-    def euclidean_distance(self, vec1, vec2):
-        return np.sum((vec1 - vec2) ** 2)
+    def __init__(self, train_set: pd.DataFrame, test_set: pd.DataFrame):
+        self.X_train, self.y_train = self.prepare_data(train_set)
+        self.X_test, self.y_test = self.prepare_data(test_set)
 
-    def get_neighbourhood(self, X_train, y_train, point, K):
-        distances = []
-        for x_train, y in zip(X_train, y_train):
-            distance = self.euclidean_distance(point, x_train)
-            distances.append((distance, y))
+    @staticmethod
+    def euclidean_distance(vec1: np.ndarray, vec2: np.ndarray) -> float:
+        return np.sqrt(np.sum((vec1 - vec2) ** 2))
+
+    @staticmethod
+    def prepare_data(dataframe: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
+        X = dataframe.iloc[:, :-1].values  # features
+        y = dataframe.iloc[:, -1].values  # labels
+        return X, y
+
+    def get_neighbourhood(self, point: np.ndarray, K: int) -> int:
+        distances = [(self.euclidean_distance(point, x_train), y) 
+                     for x_train, y in zip(self.X_train, self.y_train)]
         distances.sort(key=lambda x: x[0])
         neighbours = [y for _, y in distances[:K]]
         return mode(neighbours)
-  
-    def get_accuracy(self, pred, y_test):
-        correct = sum(p == t for p, t in zip(pred, y_test))
-        return correct / len(pred)
-    
-    def knn_main_code(self, X_train, X_test, y_train, K):
-        pred = [self.get_neighbourhood(X_train, y_train, x, K) for x in X_test]
-        return pred
+
+    def get_accuracy(self, predictions: List[int]) -> float:
+        correct = sum(p == t for p, t in zip(predictions, self.y_test))
+        return correct / len(predictions)
+
+    def knn_main_code(self, K: int) -> List[int]:
+        if K <= 0:
+            raise ValueError("K must be a positive integer")
+        if K > len(self.X_train):
+            raise ValueError("K cannot be greater than the number of training samples")
+        
+        predictions = [self.get_neighbourhood(x, K) for x in self.X_test]
+        return predictions
 
 # Example usage:
 # Assuming train_set and test_set are pandas DataFrames with the last column being the labels
 
-def prepare_data(dataframe):
-    X = dataframe.iloc[:, :-1].values  # features
-    y = dataframe.iloc[:, -1].values  # labels
-    return X, y
+def main(train_set: pd.DataFrame, test_set: pd.DataFrame, K: int):
+    knn = KNN(train_set, test_set)
+    predictions = knn.knn_main_code(K)
+    accuracy = knn.get_accuracy(predictions)
+    print(f'Accuracy: {accuracy}')
 
-# train_set and test_set should be your pandas DataFrame containing the training and testing data respectively
-# knn = KNN(train_set, test_set)
-# X_train, y_train = prepare_data(train_set)
-# X_test, y_test = prepare_data(test_set)
-
+# Uncomment and modify the following lines to use your own data
+# train_set = pd.read_csv('path_to_train_set.csv')
+# test_set = pd.read_csv('path_to_test_set.csv')
 # K = 3
-# predictions = knn.knn_main_code(X_train, X_test, y_train, K)
-# accuracy = knn.get_accuracy(predictions, y_test)
-# print(f'Accuracy: {accuracy}')
+# main(train_set, test_set, K)
